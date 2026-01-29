8c2b9b4fa74c68a11d447c7e08454f6514aab726
SatoruGojo <jalalhero@gmail.com>
2026-01-28 20:38:48 -0500

    Back burner for now. Phone number is looking good, and will essentially copy this for the zip code func.
    Need to ensure the length of input is equal to 10. And now sure what it is for zipcode. But we'll see.

--- cyopPython/4honor/jalal_lab_4.py
+++ cyopPython/4honor/jalal_lab_4.py
@@ -1,2 +1,18 @@
 '''Docstring for jalal_lab_4'''
+import re
 
+def phone_number():
+    phone_input = input('Enter in your phone number with no dashes or parentheses: ')
+    result = phone_input.isnumeric()
+    # if phone_input
+    while True:
+        if result == True: 
+            areacode = phone_input[0:3]
+            middle_number = phone_input[3:6]
+            last_four = phone_input[-4:]
+            formatted_number = (f'{areacode}-{middle_number}-{last_four}')
+            print(formatted_number)
+            break
+        if result == False:
+            print('Your number was incorrect, would you like to try again?')
+phone_number()
