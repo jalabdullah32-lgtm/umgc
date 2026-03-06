ee77e593934dc81b6df9c3999488b1bcb7687620
ippo <jalabdullah32@gmail.com>
2026-03-06 07:20:36 -0500

    don't look or talk

--- cyopPython/lab8/gahhh.py
+++ cyopPython/lab8/gahhh.py
@@ -1,14 +1,50 @@
-from difflib import *
+import difflib
+import logging
+logger = logging.getLogger(__name__)
+
+
+def do_something():
+    logger.info('Doing something')
+
+
 def common_passwords(password):
     '''checks user password against common ones'''
-    with open('CommonPasswords.txt',encoding="utf-8") as f:
-        read_data = f.read()
-    close_passwords = get_close_matches(password,[read_data])
+    with open('CommonPasswords.txt','r',encoding="utf-8") as f:
+        possibilities = [line.strip() for line in f]
 
-    if len(close_passwords) == 0:
-        return None
-    if len(close_passwords) > 0:
-        return 'The password you entered  was found in a list of common passwords'
+    n = 3 
+    cutoff = 0.7
+    close_passwords = difflib.get_close_matches(password,possibilities,n,cutoff)
+    print(close_passwords)
 
-password = 'dragonball'
+password = 'applepasswor'
 common_passwords(password)
+
+
+
+    #     file_content = ''
+    #     line = f.readline()
+
+    #     while line:
+    #         file_content += line
+    #         line.strip
+    #         line = f.readline()
+    # print(file_content)
+
+
+    # word = password
+    # possibilities = file_content
+    # n = 3 
+    # cutoff = 0.7
+    # close_passwords = difflib.get_close_matches(word,possibilities,n,cutoff)
+    # print(close_passwords)
+
+# word = "learning"
+# possibilities = ["love", "learn", "lean", "moving", "hearing"]
+# n = 3
+# cutoff = 0.7
+
+# close_matches = difflib.get_close_matches(word, 
+#                 possibilities, n, cutoff)
+
+# print(close_matches)
--- cyopPython/lab8/lab8.py
+++ cyopPython/lab8/lab8.py
@@ -1,17 +1,36 @@
 ''' lab7 flask proj'''
 import re
+import logging
+import difflib
 from flask import Flask, render_template, request, redirect, url_for,flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_session import Session
-from flask import *
 
-
-# cmd python -m flask --app test_fiele.py --debug run
+# cmd python -m flask --app lab8.py --debug run
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+def file_logger():
+    '''Provides logging for routes'''
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # file handler
+    fh = logging.FileHandler('test.log')
+    fh.setLevel(logging.DEBUG)
+
+    # formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    # handler
+    logger.addHandler(fh)
+    return logger
+logger = file_logger()
+
 
 Session(app)
 if __name__ == "__main__":
@@ -44,7 +63,36 @@ def user_is_valid(username):
     if username in read_data:
         return 'Username is already taken, please try again'
     return None
+def common_passwords(password):
+    '''checks user password against common ones'''
+    with open('CommonPasswords.txt','r', encoding="utf-8") as f:
+        words = [line.strip() for line in f]
+    n = 3
+    cutoff = 0.5
+    close_passwords = difflib.get_close_matches(password,words,n,cutoff)
 
+    if close_passwords:
+        return 'The password you entered was found in a list of common passwords'
+    return None
+def find_user(username):
+    '''looks for username, user inputs'''
+    with open('usernames.txt',encoding="utf-8") as f:
+        read_data = f.read()
+    if username not in read_data:
+        return 'Username not found'
+    return None
+def load_users():
+    '''loads users for functions'''
+
+    users = {}
+
+    with open('usernames.txt','r',encoding="utf-8") as f:
+        for line in f:
+            username,hashed_password = line.strip().split(':', 1)
+            users[username] = hashed_password
+    return users
+
+# T-tra0R6!RiM
 @app.route('/registration', methods = ['GET','POST'])
 def register():
     '''test_registration'''
@@ -56,6 +104,7 @@ def register():
 
         error = None
         invalid_password = password_is_valid(password)
+        common_password = common_passwords(password)
         invalid_username = user_is_valid(username=username)
 
         if invalid_password:
@@ -63,10 +112,16 @@ def register():
             flash(error)
             return render_template('registration.html', error=error)
 
+        if common_password:
+            error = common_password
+            flash(common_password)
+            return render_template('registration.html', error=error)
+
         if invalid_username:
             error = invalid_username
             flash(error)
             return render_template('registration.html', error=error)
+
 
         hashed_password = generate_password_hash(password)
         users[username] = hashed_password
@@ -85,37 +140,83 @@ def register():
 @app.route('/login', methods = ['GET','POST'])
 def login():
     ''' test_login'''
+    print(session)
     if request.method == "POST":
+        logger.warning("failed login attempt:")
 
         username = request.form["username"]
         password = request.form["password"]
+        users = load_users()
+
 
         if username in users and check_password_hash(users[username], password):
             session['username'] = username
-            session['logged_in'] = True
             flash("Logging in")
             return redirect(url_for('home'))
         flash("invalid username or password")
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    '''clears session'''
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/reset-password', methods =['GET','POST'])
+def password_reset():
+    '''user can reset password'''
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        error = None
+        invalid_password = password_is_valid(password)
+        user_not_found = find_user(username)
+
+        if invalid_password:
+            error = invalid_password
+            flash(error)
+            return render_template('password-reset.html', error=error)
+
+        if user_not_found:
+            error = user_not_found
+            flash(error)
+            return render_template('password-reset.html', error=error)
+
+        hashed_password = generate_password_hash(password)
+        users[username] = hashed_password
+
+        if username in users and check_password_hash(users[username], password):
+            with open('passwords.txt','a',encoding="utf-8") as f:
+                f.write(hashed_password + '\n')
+
+            with open('usernames.txt','a',encoding="utf-8") as f:
+                f.write(f"{username}:{hashed_password}\n")
+
+        flash('Reset complete')
+        return redirect(url_for('login'))
+    return render_template('password-reset.html')
+
+
 @app.route('/home')
 def home():
-    '''user clicks buttons to navigate website'''    
+    '''user clicks buttons to navigate website'''
+    print(session)
     if not session.get('username'):
-        return redirect("/login")
-    return render_template('home.html')
+        return redirect("login")
+    return render_template("home.html")
 
 @app.route('/cookies')
 def cookies():
     ''' user is displayed cookies'''
     if not session.get('username'):
-        return redirect("/login")
-
+        return redirect("login")
     return render_template('cookies.html')
 
 @app.route('/about')
 def about():
     ''' user is displayed content about author'''
     if not session.get('username'):
-        return redirect("/login")
+        return redirect("login")
     return render_template('about.html')
